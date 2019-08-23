476bb95cbd300ab7d64288fbfbfd4f3f247ab81b
Romain Morlevat <romainmorlevat@gmail.com>
2019-08-23 14:02:49 +0200

    Add validation to form

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -14,6 +14,25 @@ class CarsNew extends Component {
     });
   }
 
+  required = value => (value ? undefined : 'Required');
+
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor={`Input${field.label}`}>{field.label}</label>
+        <input
+          className="form-control"
+          placeholder={field.placeholder}
+          type={field.type}
+          {...field.input}
+          validate={field.valide}
+        />
+        {field.meta.touched && field.meta.error &&
+          <span className="error" style={{ color: "red" }}>{field.meta.error}</span>}
+      </div>
+    );
+  }
+
   render () {
     return (
       <div className="row">
@@ -22,22 +41,50 @@ class CarsNew extends Component {
         <div className="col-sm-9">
           <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
             <div className="form-group">
-              <label htmlFor="InputBrand">Brand</label>
-              <Field name="brand" type="text" placeholder="Aston Martin" component="input" className="form-control" />
+              <Field
+                component={this.renderField}
+                label="Brand"
+                name="brand"
+                placeholder="Aston Martin"
+                type="text"
+                validate={[this.required]}
+                warn={this.required}
+              />
             </div>
             <div className="form-group">
-              <label htmlFor="InputModel">Model</label>
-              <Field name="model" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+              <Field
+                component={this.renderField}
+                label="Model"
+                name="model"
+                placeholder="DB7"
+                type="text"
+                validate={[this.required]}
+                warn={this.required}
+              />
             </div>
             <div className="form-group">
-              <label htmlFor="InputOwner">Owner</label>
-              <Field name="owner" type="text" placeholder="James Bond" component="input" className="form-control" />
+              <Field
+                component={this.renderField}
+                label="Owner"
+                name="owner"
+                placeholder="James Bond"
+                type="text"
+                validate={[this.required]}
+                warn={this.required}
+              />
             </div>
             <div className="form-group">
-              <label htmlFor="InputPlate">Plate</label>
-              <Field name="plate" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+              <Field
+                component={this.renderField}
+                label="Plate"
+                name="plate"
+                placeholder="007"
+                type="text"
+                validate={[this.required]}
+                warn={this.required}
+              />
             </div>
-            <button type="submit" className="btn btn-primary">Add car</button>
+            <button type="submit" className="btn btn-primary" disabled={this.props.invalid || this.props.pristine || this.props.submitting}>Add car</button>
           </form>
         </div>
       </div>
